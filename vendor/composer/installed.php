20cbdfa36cac261ab692152ff1027d75a4cf8bc6
MichaelVento <michael.vento@everysoft.be>
2024-08-27 06:21:21 +0200

    test

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ventom/flutter-api-package',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'c00d6a5c37e6e73f4c149bd9fdbb47ff3041a566',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ventom/flutter-api-package' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'c00d6a5c37e6e73f4c149bd9fdbb47ff3041a566',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
